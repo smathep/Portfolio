cc7471017f04fc62aff1d63af567736c476e4364
Patrick Smathers <patricksmathers@icloud.com>
2023-09-20 00:02:17 -0400

    Rebuilt photography page. Dynamically show pictures

--- src/pages/Photography.jsx
+++ src/pages/Photography.jsx
@@ -1,47 +1,8 @@
-export default function Photography(){
+import ImageGallery from "./ImageGallery"
+export default function Photography_Test(){
     return (
         <div>
-            <br/>
-            <br/>
-            <h1>Photography</h1>
-            {/* <br/> */}
-            <p>Photography is a nice side hobby of mine. I generally do landscape or nature photography, but portait photography is something I want to try. Sadly, it'a something that I don't have a lot of time for, but I try to make the most of it when I do. I shoot with a Cannon Rebel T8i for most pictures you'll see here, but some may be take either with a Cannon T6, iPhone 7, or iPhone 13 Pro. </p>
-            <p>This is a small subset of the photos I have taken, so if you'd like to see more, go <a id="links" href="https://photos.app.goo.gl/pYjoG73f0xN4Y2By1">here to check out some more pictures!</a> </p>
-
-            <div class="row">
-                <div class="column">
-                    <a href="./assets/photography/highRes/_MG_3845-HDR.jpg"> <img src="./assets/photography/proxies/_MG_3845-HDR.jpg"/></a>
-                    <a href ="./assets/photography/highRes/IMG_0153.jpg"> <img src="./assets/photography/proxies/IMG_0153.jpg"/></a>
-                    <a href="./assets/photography/highRes/APC_0352-hdr.jpg"> <img src="./assets/photography/proxies/APC_0352-hdr.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_1326.jpg"> <img src="./assets/photography/proxies/IMG_1326.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_1260-Pano.jpg"> <img src="./assets/photography/proxies/IMG_1260-Pano.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_1395-Pano.jpg"> <img src="./assets/photography/proxies/IMG_1395-Pano.jpg"/></a>
-                </div>
-                <div class="column">
-                    <a href="./assets/photography/highRes/IMG_3303.jpg"> <img src="./assets/photography/proxies/IMG_3303.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_3369.jpg"> <img src="./assets/photography/proxies/IMG_3369.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_1309.jpg"> <img src="./assets/photography/proxies/IMG_1309.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_1631-Pano.jpg"> <img src="./assets/photography/proxies/IMG_1631-Pano.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_1363.jpg"> <img src="./assets/photography/proxies/IMG_1363.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_1713.jpg"> <img src="./assets/photography/proxies/IMG_1713.jpg"/></a>
-                </div>
-                <div class="column">
-                    <a href="./assets/photography/highRes/IMG_3384.jpg"> <img src="./assets/photography/proxies/IMG_3384.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_5775-HDR.jpg"> <img src="./assets/photography/proxies/IMG_5775-HDR.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_2054.jpg"> <img src="./assets/photography/proxies/IMG_2054.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_2403.jpg"> <img src="./assets/photography/proxies/IMG_2403.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_2410.jpg"> <img src="./assets/photography/proxies/IMG_2410.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_2412.jpg"> <img src="./assets/photography/proxies/IMG_2412.jpg"/></a>
-                </div>
-                <div class="column">
-                    <a href="./assets/photography/highRes/IMG_5869.jpg"> <img src="./assets/photography/proxies/IMG_5869.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_5895.jpg"> <img src="./assets/photography/proxies/IMG_5895.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_2422.jpg"> <img src="./assets/photography/proxies/IMG_2422.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_2440.jpg"> <img src="./assets/photography/proxies/IMG_2440.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_4092.jpg"> <img src="./assets/photography/proxies/IMG_4092.jpg"/></a>
-                    <a href="./assets/photography/highRes/IMG_8383.jpg"> <img src="./assets/photography/proxies/IMG_8383.jpg"/></a>
-                </div>
-            </div>
+           <ImageGallery></ImageGallery>
         </div>
     )
 }
