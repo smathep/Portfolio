30caf9e28be460864f82984410c9d029c4856757
Patrick Smathers <patricksmathers@icloud.com>
2024-02-19 11:54:59 -0500

    Updated image paths and logic

--- src/app/components/ImageGallery/ImageGallery.component.ts
+++ src/app/components/ImageGallery/ImageGallery.component.ts
@@ -1,29 +1,49 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit, inject } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { CommonModule } from "@angular/common";
-// import { NavbarComponent } from "../components/navbar/navbar.component";
+import { HttpClientModule } from "@angular/common/http";
+import { PortfolioServiceService } from "../../portfolio-service.service";
 
 @Component({
-    selector: 'projects',
+    selector: 'imageGallery',
     standalone: true,
-    imports: [CommonModule],
-    templateUrl: './projects.component.html',
-    styleUrls: ['./projects.component.scss']
-    
+    imports: [CommonModule, HttpClientModule],
+    templateUrl: './ImageGallery.component.html',
+    styleUrls: ['./ImageGallery.component.scss'],
+    providers: [PortfolioServiceService]
   })
-  export class ProjectsComponent {
-    title = 'projects';
-}
 
 export class ImageGalleryComponent implements OnInit{
-    ImagesPath: string;
+    @Input() S3FolderPath!: string;
+    ImageFileNames: any;
 
-    constructor() {
-        this.ImagesPath = "assets/images/";
+    private service = inject(PortfolioServiceService);
+
+    constructor() {}
+
+    showImages(){
         
+        //console.log(this.ImageFileNames);
+    }
+
+    getImageList(){
+        this.service.getImageFileNames()
+        .subscribe({
+            next: (data) => {
+                this.ImageFileNames = data;
+            }
+        }
+        );
+        return this.ImageFileNames;
+        //console.log(this.ImageList);
+    }
+
+    openWindow(image: any) {
+        window.open(image);
     }
 
     ngOnInit(): void {
-        
+        this.ImageFileNames = this.getImageList();
     }
 
 }
