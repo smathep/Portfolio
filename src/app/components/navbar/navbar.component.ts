b26ac4f6230c2b6ee819c15811df8c2208bd917c
Patrick Smathers <patricksmathers@icloud.com>
2026-02-17 09:22:57 -0500

    Revamped webstie with copilot

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'navbar-menu',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isScrolled = false;
+  menuOpen = false;
 
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 20;
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,12 +1,12 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterOutlet } from "@angular/router";
+import { RouterOutlet, RouterLink } from "@angular/router";
 import { NavbarComponent } from "../components/navbar/navbar.component";
 
 @Component({
     selector: 'resume',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, NavbarComponent],
+    imports: [CommonModule, RouterOutlet, RouterLink, NavbarComponent],
     templateUrl: './resume.component.html',
     styleUrls: ['./resume.component.scss']
     
